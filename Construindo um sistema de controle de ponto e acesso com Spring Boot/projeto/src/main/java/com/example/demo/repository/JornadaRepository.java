05b507642bc4ca250d124827def0c2addfc7df79
Raphael Malta <86579727+Raphael-Malta@users.noreply.github.com>
2021-08-21 18:13:29 -0300

    Projeto
    Projeto Digital Innovation One

---
+++ Construindo um sistema de controle de ponto e acesso com Spring Boot/projeto/src/main/java/com/example/demo/repository/JornadaRepository.java
@@ -0,0 +1,9 @@
+package com.example.demo.repository;
+
+import com.dio.live.model.JornadaTrabalho;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface JornadaRepository extends JpaRepository<JornadaTrabalho, Long> {
+}
